74097168479dfc6bc1eca8c54f868842d719c9a1
mgalcudia <mgalcudia@users.noreply.github.com>
2020-03-16 18:02:19 +0100

    Mensaje de bienvenida y margenes botones

---
+++ app/src/main/java/com/proyecto/Certificado/AgregarActivity.java
@@ -0,0 +1,14 @@
+package com.proyecto.Certificado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar);
+    }
+}
