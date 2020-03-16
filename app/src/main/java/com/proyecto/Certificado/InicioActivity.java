fe36cf97c4a5a95782c44e1600419f8b06a8b48a
mgalcudia <mgalcudia@users.noreply.github.com>
2020-03-16 01:01:08 +0100

    botones ventana inicio

--- app/src/main/java/com/proyecto/Certificado/InicioActivity.java
+++ app/src/main/java/com/proyecto/Certificado/InicioActivity.java
@@ -18,55 +18,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class InicioActivity extends AppCompatActivity {
-    private Button mButtonSingout;
-    private TextView mTextViewName;
-    private TextView mTextViewEmail;
-    private TextView mTextViewIdUser;
-    private FirebaseAuth mAuth;
-    private DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
-        mAuth= FirebaseAuth.getInstance();
-        mDatabase= FirebaseDatabase.getInstance().getReference();
-        mButtonSingout = (Button) findViewById(R.id.btnCerrarSesion);
-        mTextViewName = (TextView) findViewById(R.id.textViewName);
-        mTextViewEmail = (TextView) findViewById(R.id.textViewEmail);
-        mTextViewIdUser = (TextView) findViewById(R.id.textViewIdUser);
-        getUserInfo();
-    }
-
-    private void getUserInfo() {
-
-        final String id= mAuth.getCurrentUser().getUid();
-
-        mDatabase.child("user").child(id).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
-
-                    String name= dataSnapshot.child("name").getValue().toString();
-                    String email= dataSnapshot.child("email").getValue().toString();
-
-                    mTextViewName.setText(name);
-                    mTextViewEmail.setText(email);
-                    mTextViewIdUser.setText(id);
-
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    public void cerrarSesion(View view) {
-        mAuth.signOut();
-        startActivity(new Intent(InicioActivity.this,MainActivity.class));
-        finish();
 
     }
+
+
 }
--- app/src/main/java/com/proyecto/Certificado/MainActivity.java
+++ app/src/main/java/com/proyecto/Certificado/MainActivity.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth= FirebaseAuth.getInstance();
 
     }
 
@@ -21,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void irLoguin(View view) {
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        if(mAuth.getCurrentUser() !=null){
+
+            startActivity(new Intent(MainActivity.this,InicioActivity.class));
+            finish();
+        }
     }
 }
