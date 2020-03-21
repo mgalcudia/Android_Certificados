6ad2f4f62939b9aad51cba8c206f75bc1c910a9d
mgalcudia <mgalcudia@users.noreply.github.com>
2020-03-21 18:48:56 +0100

    Datepicker Fecha fin realizado

--- app/src/main/java/com/proyecto/Certificado/AgregarActivity.java
+++ app/src/main/java/com/proyecto/Certificado/AgregarActivity.java
@@ -1,59 +1,70 @@
 package com.proyecto.Certificado;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-
 import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.icu.util.Calendar;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+import java.util.Locale;
+
 public class AgregarActivity extends AppCompatActivity {
+    //Iniciamos variable fechafinal
+    private EditText fechaFin;
+    private int dia,mes,anio;
+
+    //Creacion de listener para el datepicker
+    private DatePickerDialog.OnDateSetListener listenerDatepicker= new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            //Se llama cuando se selecciona la fecha. Nos pasa la vista y asignamos valos a las variables
+            anio= year;
+            mes= month;
+            dia= dayOfMonth;
+            //Refrescamos la fecha
+            refrescarFechaEnEditText();
+
+        }
+    };
+
+
+    public void refrescarFechaEnEditText(){
+
+        String fecha= String.format(Locale.getDefault(),"%02d-%02d-%02d", dia, mes+1, anio);
+        //asigamos el valor del editText
+
+        fechaFin.setText(fecha);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar);
-        EditText etPlannedDate = (EditText) findViewById(R.id.etPlannedDate);
-        etPlannedDate.setOnClickListener((View.OnClickListener) this);
+        //instanciamos el objeto
+        fechaFin= findViewById(R.id.editTextFechaFin);
+        //Asignamos la fecha de hoy al editText
+        final Calendar calendario= Calendar.getInstance();
+        dia= calendario.get(Calendar.DAY_OF_MONTH);
+        mes= calendario.get(Calendar.MONTH);
+        anio= calendario.get(Calendar.YEAR);
+        //Asignamos la fecha de hoy al EditText
+        refrescarFechaEnEditText();
+
+        //Hace que el datepicker se muestre al clickear el edixText
+
+        fechaFin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialogoDeFecha = new DatePickerDialog(AgregarActivity.this,listenerDatepicker,anio,mes,dia);
+
+                //Muestra
+                dialogoDeFecha.show();
+            }
+        });
+
     }
 
-
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.etPlannedDate:
-                showDatePickerDialog();
-                break;
-        }
-    }
-    private void showDatePickerDialog() {
-        DatePickerFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
-    }
-    public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-
-        @RequiresApi(api = Build.VERSION_CODES.N)
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
-        }
-
-
-        @Override
-        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-
-        }
-    }
 }
